aeb382da43345a811caa955ff10572ad04c3b552
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-05-05 21:33:35 +0200

    Drone.model.js

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const { Schema, model,} = require('mongoose');
+
+const droneSchema = new Schema ({
+    name: {
+        type: String,
+        require: true
+    },
+    propellers: {
+        type: Number,
+        requite: true
+    },
+    maxSpeed: {
+        type: Number,
+        require: true
+    }
+})
+
+const Drone = model('Drone', droneSchema);
+
+module.exports = Drone;
